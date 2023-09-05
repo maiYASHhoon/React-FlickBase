2a5efb7a7352526929bf554199ff8ac9f932de9c
yashdayama <yash.dayama@imperoit.com>
2023-09-05 10:45:42 +0530

    Main Layout

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,20 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
+import MainLayout from './hoc/mainLayout';
 import Header from './components/navigation/header';
 import Home from './components/home';
-
 const Router = () => {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={<Home />}
-        />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+        </Routes>
+      </MainLayout>
     </BrowserRouter>
   );
 };
-
 export default Router;
