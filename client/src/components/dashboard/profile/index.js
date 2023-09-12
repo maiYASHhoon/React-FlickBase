3cfd84545e0aecf8b9a74823fbb41619fb1977ec
yashdayama <yash.dayama@imperoit.com>
2023-09-12 11:38:20 +0530

    Update-Email

--- client/src/components/dashboard/profile/index.js
+++ client/src/components/dashboard/profile/index.js
@@ -1,9 +1,11 @@
 import { AdminTitle } from '../../../utils/tools';
+import AuthProfile from './auth';
 import UserProfile from './profile';
 const AdminProfile = () => {
   return (
     <>
       <AdminTitle title="Profile" />
+      <AuthProfile />
       <UserProfile />
     </>
   );
